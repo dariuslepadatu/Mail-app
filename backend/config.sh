f05c6aa413627d84b1efdbb5269fe49bcd89299b
Lepădatu Darius <94700118+dariuslepadatu@users.noreply.github.com>
2024-01-19 13:58:21 +0200

    Update config.sh

--- backend/config.sh
+++ backend/config.sh
@@ -1,3 +1,3 @@
-echo "export SENDGRID_API_KEY='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'" > sendgrid.env
+echo "export SENDGRID_API_KEY='your_key'" > sendgrid.env
 echo "sendgrid.env" >> .gitignore
 source ./sendgrid.env
